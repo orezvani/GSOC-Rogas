5b1e230e838592dc61ccf769b8e7ad99d8016249
orezvani <orezvani@gmail.com>
2016-07-29 22:16:33 +1000

    Created an appropriate class for ktrusses; Here we need a weighted graph with O(1) access to edges. We used a dictionary data structure to mimic a sparse matrix.

--- ktruss.py
+++ ktruss.py
@@ -1,1 +1,100 @@
-# Started k-truss community detection and local community search
+#!/usr/bin/env python
+
+#################################################
+#   This code finds k-truss of a given graph    #
+#   It also queries vertices in k-trusses       #
+#   Author: Mojtaba (Omid) Rezvani              #
+#################################################
+
+import sys
+from os.path import isfile, join
+
+class Vertex:
+    def __init__(self, node):
+        self.id = node
+        self.adjacent = {}
+
+    def __str__(self):
+        return str(self.id) + ' adjacent: ' + str([x.id for x in self.adjacent])
+
+    def add_neighbor(self, neighbor, weight=0):
+        self.adjacent[neighbor] = weight
+
+    def get_connections(self):
+        return self.adjacent.keys()  
+
+    def get_id(self):
+        return self.id
+
+    def get_weight(self, neighbor):
+        return self.adjacent[neighbor]
+
+
+class Graph:
+    def __init__(self):
+        self.vert_dict = {}
+        self.num_vertices = 0
+
+    def read_graph(self, graph_file):
+        """ Add connections (list of tuple pairs) to graph """
+        with open(graph_file) as gf:
+            for line in gf:
+                    e = [int(v) for v in line]
+                    add_edge(e[0], e[1])
+        gf.close()
+
+    def __iter__(self):
+        return iter(self.vert_dict.values())
+
+    def add_vertex(self, node):
+        self.num_vertices = self.num_vertices + 1
+        new_vertex = Vertex(node)
+        self.vert_dict[node] = new_vertex
+        return new_vertex
+
+    def get_vertex(self, n):
+        if n in self.vert_dict:
+            return self.vert_dict[n]
+        else:
+            return None
+
+    def add_edge(self, frm, to, cost = 0):
+        """ Add connection between frm and to """
+
+        if frm not in self.vert_dict:
+            self.add_vertex(frm)
+        if to not in self.vert_dict:
+            self.add_vertex(to)
+
+        self.vert_dict[frm].add_neighbor(self.vert_dict[to], cost)
+        self.vert_dict[to].add_neighbor(self.vert_dict[frm], cost)
+
+    def print_edges(self):
+        for v in self:
+            for w in v.get_connections():
+                vid = v.get_id()
+                wid = w.get_id()
+                print '( %s , %s, %3d)'  % ( vid, wid, v.get_weight(w))
+
+    def print_graph(self):
+        for v in self:
+            print 'g.vert_dict[%s]=%s' %(v.get_id(), self.vert_dict[v.get_id()])
+
+    def is_connected(self, node1, node2):
+        if node1 in self.vert_dict and node2 in self.vert_dict:
+            return self.vert_dict[node1].adjacent.has_key(self.vert_dict[node2])
+        else:
+            return False
+
+    def get_weight(self, node1, node2):
+        if node1 in self.vert_dict and node2 in self.vert_dict:
+            return self.vert_dict[node1].adjacent.get(self.vert_dict[node2])
+        else:
+            return -1
+
+    def get_vertices(self):
+        return self.vert_dict.keys()
+
+
+g = Graph()
+g.read_graph("edges.txt")
