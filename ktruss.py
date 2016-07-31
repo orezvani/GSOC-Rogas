73770da553b7094122c36ac664b8447974fcdad5
u5410055 <orezvani@gmail.com>
2016-07-31 12:31:22 +1000

    test

--- ktruss.py
+++ ktruss.py
@@ -13,6 +13,7 @@ class Vertex:
     def __init__(self, node):
         self.id = node
         self.adjacent = {}
+        print ""
 
     def __str__(self):
         return str(self.id) + ' adjacent: ' + str([x.id for x in self.adjacent])
@@ -35,16 +36,20 @@ class Graph:
         self.vert_dict = {}
         self.num_vertices = 0
 
+
     def read_graph(self, graph_file):
         """ Add connections (list of tuple pairs) to graph """
+
         with open(graph_file) as gf:
             for line in gf:
-                    e = [int(v) for v in line]
-                    add_edge(e[0], e[1])
+                e = [int(v) for v in line.split()]
+                self.add_edge(e[0], e[1])
         gf.close()
+
 
     def __iter__(self):
         return iter(self.vert_dict.values())
+
 
     def add_vertex(self, node):
         self.num_vertices = self.num_vertices + 1
@@ -52,11 +57,13 @@ class Graph:
         self.vert_dict[node] = new_vertex
         return new_vertex
 
+
     def get_vertex(self, n):
         if n in self.vert_dict:
             return self.vert_dict[n]
         else:
             return None
+
 
     def add_edge(self, frm, to, cost = 0):
         """ Add connection between frm and to """
@@ -69,6 +76,7 @@ class Graph:
         self.vert_dict[frm].add_neighbor(self.vert_dict[to], cost)
         self.vert_dict[to].add_neighbor(self.vert_dict[frm], cost)
 
+
     def print_edges(self):
         for v in self:
             for w in v.get_connections():
@@ -76,9 +84,11 @@ class Graph:
                 wid = w.get_id()
                 print '( %s , %s, %3d)'  % ( vid, wid, v.get_weight(w))
 
+
     def print_graph(self):
         for v in self:
             print 'g.vert_dict[%s]=%s' %(v.get_id(), self.vert_dict[v.get_id()])
+
 
     def is_connected(self, node1, node2):
         if node1 in self.vert_dict and node2 in self.vert_dict:
@@ -86,15 +96,65 @@ class Graph:
         else:
             return False
 
+
     def get_weight(self, node1, node2):
         if node1 in self.vert_dict and node2 in self.vert_dict:
             return self.vert_dict[node1].adjacent.get(self.vert_dict[node2])
         else:
             return -1
 
+
+    def update_weight(self, node1, node2, new_weight):
+        if g.is_connected(node1, node2):
+            self.vert_dict[node1].adjacent[self.vert_dict[node2]] = new_weight
+            return self.vert_dict[node1].adjacent.get(self.vert_dict[node2])
+        else:
+            return -1
+
+
+
     def get_vertices(self):
         return self.vert_dict.keys()
 
 
+    def detect_ktruss(self, k):
+        # Here we detect the k-truss
+
+        # Let's count the number of triangles
+        for v in self:
+            for w in v.get_connections():
+                vid = v.get_id()
+                wid = w.get_id()
+
+
+
+
+
 g = Graph()
-g.read_graph("edges.txt")
+g.add_vertex('a')
+g.add_vertex('b')
+g.add_vertex('c')
+g.add_vertex('d')
+g.add_vertex('e')
+g.add_vertex('f')
+
+g.add_edge('a', 'b', 7)  
+g.add_edge('a', 'c', 9)
+g.add_edge('a', 'f', 14)
+g.add_edge('b', 'c', 10)
+g.add_edge('b', 'd', 15)
+g.add_edge('c', 'd', 11)
+g.add_edge('c', 'f', 2)
+g.add_edge('d', 'e', 6)
+g.add_edge('e', 'f', 9)
+
+g.print_graph()
+print g.get_weight('a', 'b')
+g.update_weight('a', 'b', 8)
+print g.get_weight('a', 'b')
+g.update_weight('a', 'b', 9)
+print g.get_weight('a', 'b')
+g.update_weight('a', 'g', 9)
+print g.get_weight('a', 'g')
+
+
